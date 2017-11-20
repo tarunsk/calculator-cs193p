a411cdfe5ac88895230a4e2673be0c8c6d3cc83c
Tarun Khubchandani <tarun@kfit.com>
2017-11-19 23:12:39 -0500

    Completed lecture 2

--- stanford-calc-ios/ViewController.swift
+++ stanford-calc-ios/ViewController.swift
@@ -34,17 +34,19 @@ class ViewController: UIViewController {
         }
     }
     
+    private var brain: CalculatorBrain = CalculatorBrain()
+    
     @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping    {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
         userIsInTheMiddleOfTyping = false
         if let mathematicalSymbol = sender.currentTitle {
-            switch mathematicalSymbol   {
-            case "π":
-                displayValue = Double.pi
-            case "√":
-                displayValue = sqrt(displayValue)
-            default:
-                break
-            }
+            brain.performOperation(mathematicalSymbol)
+        }
+        if let result = brain.result    {
+            displayValue = result
         }
     }
     
